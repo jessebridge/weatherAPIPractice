ff9a5044d438b88098c1f66fe7eb7a843a977518
jessebridge <jesse.bridge@my.jcu.edu.au>
2018-10-12 18:35:28 +1000

    Removed unused statement and fixed code so it would show new places

--- weatherAPIcall.py
+++ weatherAPIcall.py
@@ -16,7 +16,6 @@ def get_wind_speed(json_data):
 
 
 def get_weather_data(json_data, city):
-    description_of_weather = json_data['weather'][0]['description']
     weather_type = get_weather_type(json_data)
     temperature = get_temperature(json_data)
     wind_speed = get_wind_speed(json_data)
@@ -25,10 +24,10 @@ def get_weather_data(json_data, city):
 
 
 def main():
-    api_address = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=a10fd8a212e47edf8d946f26fb4cdef8&q='
     city = input("City Name : ")
     units_format = "&units=metric"
-    final_url = api_address + city + units_format
+    api_address = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a10fd8a212e47edf8d946f26fb4cdef8&q='
+    final_url = api_address + units_format
     json_data = requests.get(final_url).json()
     weather_details = get_weather_data(json_data, city)
     # print the json_object
